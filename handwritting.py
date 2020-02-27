c05d921c57eb152e8e5ef112ca7f96846d749fb6
mohdali239 <61471324+mohdali239@users.noreply.github.com>
2020-02-27 22:16:51 +0530

    Add files via upload

---
+++ handwritting.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 18 23:57:16 2020
+
+@author: Muhammad Sarim
+"""
+
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
+import tensorflow as tf
+from keras.optimizers import SGD
+
+# the data, split between train and test sets
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+print(x_train.shape, y_train.shape)
+
+x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
+x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
+input_shape = (28, 28, 1)
+
+# convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes=None)
+y_test = keras.utils.to_categorical(y_test, num_classes=None)
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+
+#Create the model
+batch_size = 128
+num_classes = 10
+epochs = 10
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3),activation='relu',input_shape=input_shape))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
+opt = SGD(lr=0.01, momentum=0.9)
+model.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
+
+#Train the model
+hist = model.fit(x_train, y_train,batch_size=batch_size,epochs=epochs,verbose=1,validation_data=(x_test, y_test))
+print("The model has successfully trained")
+model.save('mnist.h5')
+print("Saving the model as mnist.h5")
+
+#Evaluate the model
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+
